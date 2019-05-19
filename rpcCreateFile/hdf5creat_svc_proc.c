aa21018a49f7d070302ffc227fc19bac8258003e
Aaron Chu <xweichu@hotmail.com>
2019-05-18 21:47:19 -0700

    change pointer issue

--- rpcCreateFile/hdf5creat_svc_proc.c
+++ rpcCreateFile/hdf5creat_svc_proc.c
@@ -2,7 +2,8 @@
 #include "hdf5creat.h"
 
 int * creat_file_1_svc(char ** name, struct svc_req * req){
-	H5Fcreate(*name, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+	hid_t file_id = H5Fcreate(*name, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+	H5Fclose(file_id);
 	static int result = 0 ;
 	return &result;
 }
