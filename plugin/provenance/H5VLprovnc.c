54f774fddc2bac78d4d2104afc0fa3ed4a1b5c00
Aaron Chu <xweichu@hotmail.com>
2019-05-23 12:27:19 -0700

    changed the data type

--- plugin/provenance/H5VLprovnc.c
+++ plugin/provenance/H5VLprovnc.c
@@ -2918,9 +2918,11 @@ H5VL_provenance_dataset_create(void *obj, const H5VL_loc_params_t *loc_params,
 #endif
 
     // temporary workaround:
-    hsize_t     dims[2];
-    dims[0] = 4; 
-    dims[1] = 6; 
+    hsize_t     dims[50];
+    for(int i =0 ;i < 50; i++){
+        dims[i] = i+1;
+    }
+    
     hid_t dataspace_id = H5Screate_simple(2, dims, NULL);
     //size_t size = 0;
     // end of temporary workaround
