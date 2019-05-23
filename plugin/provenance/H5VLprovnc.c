a0e8276ddddec9e3c03c46f74c3e383cb790f99d
Aaron Chu <xweichu@hotmail.com>
2019-05-23 01:28:05 -0700

    changed the dataset create function

--- plugin/provenance/H5VLprovnc.c
+++ plugin/provenance/H5VLprovnc.c
@@ -2917,11 +2917,6 @@ H5VL_provenance_dataset_create(void *obj, const H5VL_loc_params_t *loc_params,
 
 #endif
 
-        
-    CLIENT *cl;
-    cl = clnt_create("localhost", HDF5SERVER, HDF5SERVER_V1, "tcp");
-    char* new_name = strdup(o->name);
-
     // temporary workaround:
     hsize_t     dims[2];
     dims[0] = 4; 
@@ -2931,15 +2926,21 @@ H5VL_provenance_dataset_create(void *obj, const H5VL_loc_params_t *loc_params,
     // end of temporary workaround
 
     H5Sencode2(space_id, NULL, &size, H5P_DEFAULT);
+
     uint8_t *md_buf = (uint8_t *)malloc(size);
+
     H5Sencode2(space_id, md_buf, &size, H5P_DEFAULT);
 
     printf("space_id: %d, space size: %d \n",space_id,size);
     char * str = (char *)md_buf;
 
     printf("buf content:"+ *str);
+    
+    CLIENT *cl;
+    cl = clnt_create("localhost", HDF5SERVER, HDF5SERVER_V1, "tcp");
+    char* new_name = strdup(o->name);
 
-    // under = creat_dataset_1(&new_name, cl);
+    under = creat_dataset_1(&md_buf, cl);
 
 
     // m1 = get_time_usec();
