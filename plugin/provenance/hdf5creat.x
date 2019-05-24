49e012b9378973db56dae23e637113f65ffa3974
Aaron Chu <xweichu@hotmail.com>
2019-05-24 00:51:36 -0700

    update

--- plugin/provenance/hdf5creat.x
+++ plugin/provenance/hdf5creat.x
@@ -12,7 +12,7 @@ version HDF5SERVER_V1{
     int creat_file(string) = 1; 
     int creat_dataset(list) = 2;
     int open_dataset(list) =3;
-    int open_file(list) =4;
+    int open_file(string) =4;
     }=1;
 
 }=0x2fffffff;
--- plugin/provenance/hdf5creat_svc_proc.c
+++ plugin/provenance/hdf5creat_svc_proc.c
@@ -42,18 +42,8 @@ int * open_dataset_1_svc(list * lst, struct svc_req * req){
 	return &result;
 }
 
-int * open_file_1_svc(list * lst, struct svc_req * req){
-	list *ptr;
-    ptr = lst;
-	
-	hid_t file_id = H5Fopen(ptr->name,H5F_ACC_RDWR,H5P_DEFAULT);
-	hid_t dataspace = H5Sdecode(ptr->data.data_val);
-	printf("dataspace:%d \n", (int)dataspace);
-	hid_t dataset_id = H5Dcreate(file_id, ptr->dsname, H5T_STD_I32BE, dataspace, 
-                          H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-	
-    H5Dclose(dataset_id);
-	H5Sclose(dataspace);
+int * open_file_1_svc(char ** name, struct svc_req * req){
+	hid_t file_id = H5Fcreate(*name, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
 	H5Fclose(file_id);
 	static int result = 0;
 	return &result;
