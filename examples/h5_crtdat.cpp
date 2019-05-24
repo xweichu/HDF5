c897d1a2f74944c0c9577fc7941db68754fa15e1
Aaron Chu <xweichu@hotmail.com>
2019-05-23 18:08:27 -0700

    update

--- examples/h5_crtdat.cpp
+++ examples/h5_crtdat.cpp
@@ -49,8 +49,8 @@ int main() {
    // printf("size is %d \n", size);
 
    /* Create the dataset. */
-   dataset_id = H5Dcreate(file_id, "/dset2", H5T_STD_I32BE, dataspace_id, 
-                          H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+   // dataset_id = H5Dcreate(file_id, "/dset2", H5T_STD_I32BE, dataspace_id, 
+   //                        H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 
    
    printf("test\n");
