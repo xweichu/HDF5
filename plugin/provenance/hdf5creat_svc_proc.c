fd4aa195bf9705a9c4308d5e266614e063433504
Aaron Chu <xweichu@hotmail.com>
2019-05-24 00:55:54 -0700

    update

--- plugin/provenance/hdf5creat_svc_proc.c
+++ plugin/provenance/hdf5creat_svc_proc.c
@@ -30,20 +30,16 @@ int * open_dataset_1_svc(list * lst, struct svc_req * req){
     ptr = lst;
 	
 	hid_t file_id = H5Fopen(ptr->name,H5F_ACC_RDWR,H5P_DEFAULT);
-	hid_t dataspace = H5Sdecode(ptr->data.data_val);
-	printf("dataspace:%d \n", (int)dataspace);
-	hid_t dataset_id = H5Dcreate(file_id, ptr->dsname, H5T_STD_I32BE, dataspace, 
-                          H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+	hid_t dataset_id = H5Dopen2(file_id, ptr->dsname, H5P_DEFAULT);
 	
     H5Dclose(dataset_id);
-	H5Sclose(dataspace);
 	H5Fclose(file_id);
 	static int result = 0;
 	return &result;
 }
 
 int * open_file_1_svc(char ** name, struct svc_req * req){
-	hid_t file_id = H5Fcreate(*name, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+	hid_t file_id = H5Fopen(*name, H5F_ACC_RDWR, H5P_DEFAULT);
 	H5Fclose(file_id);
 	static int result = 0;
 	return &result;
