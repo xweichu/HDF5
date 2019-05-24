16b1648c2cce5164b39849fe72a91536d90de86b
Aaron Chu <xweichu@hotmail.com>
2019-05-24 01:57:03 -0700

    update

--- plugin/provenance/hdf5creat_svc_proc.c
+++ plugin/provenance/hdf5creat_svc_proc.c
@@ -60,10 +60,10 @@ list * read_dataset_1_svc(list * lst, struct svc_req * req){
 	hid_t file_id = H5Fopen(ptr->name,H5F_ACC_RDWR,H5P_DEFAULT);
 	hid_t dataset_id = H5Dopen2(file_id, ptr->dsname, H5P_DEFAULT);
 
-	uint8_t* dset_data = (uint8_t*)malloc(5*sizeof(int));
+	int* dset_data = (int*)malloc(5*sizeof(int));
 	H5Dread(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, 
                     dset_data);
-					
+
 	res->data.data_len = 5*sizeof(int);
 	res->data.data_val = dset_data;
 
