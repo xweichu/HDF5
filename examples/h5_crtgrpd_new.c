8f58e7abf13e2172f795275bc4d4a08864f47c25
Aaron Chu <xweichu@hotmail.com>
2019-06-03 16:02:06 -0700

    update

---
+++ examples/h5_crtgrpd_new.c
@@ -0,0 +1,61 @@
+
+#include "hdf5.h"
+#include <time.h>
+#define FILE "groups.h5"
+#define DATASIZE 1100
+
+int main() {
+
+   unsigned long start = get_time_usec();
+   hid_t       file_id, dataset_id, dataspace_id;  /* identifiers */
+   hsize_t     dims[2];
+   herr_t      status;
+   int         i, j, dset1_data[DATASIZE][DATASIZE];
+
+   /* Initialize the first dataset. */
+   for (i = 0; i < DATASIZE; i++)
+      for (j = 0; j < DATASIZE; j++)
+         dset1_data[i][j] = j + i + 1;
+
+   /* Create a file. */
+   file_id = H5Fcreate(FILE, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+
+   unsigned long stop = get_time_usec();
+   printf("time to create the file: %d", stop - start);
+   
+   start = get_time_usec();
+
+   /* Create the data space for the first dataset. */
+   dims[0] = DATASIZE;
+   dims[1] = DATASIZE;
+
+
+
+
+
+   /* Create a dataset in group "MyGroup". */
+   dataset_id = H5Dcreate2(file_id, "/dset1", H5T_STD_I32BE, dataspace_id,
+                          H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+   stop = get_time_usec();
+   
+   printf("time to create the dataset: %d", stop - start);
+
+   start = get_time_usec();
+
+   /* Write the first dataset. */
+   status = H5Dwrite(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT,
+                     dset1_data);
+
+
+    stop = get_time_usec();
+       printf("time to write the dataset: %d", stop - start);
+   /* Close the data space for the first dataset. */
+   status = H5Sclose(dataspace_id);
+
+   /* Close the first dataset. */
+   status = H5Dclose(dataset_id);
+
+   /* Close the file. */
+   status = H5Fclose(file_id);
+}
+
