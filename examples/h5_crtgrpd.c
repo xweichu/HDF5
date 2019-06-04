eec70c077e901741c137b0b48d1d12d304017354
Aaron Chu <xweichu@hotmail.com>
2019-06-03 18:26:31 -0700

    update

--- examples/h5_crtgrpd.c
+++ examples/h5_crtgrpd.c
@@ -18,12 +18,11 @@ int main() {
    /* Create a file. */
    file_id = H5Fcreate(FILE, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
 
-   /* Create the data space for the first dataset. */
-   dims[0] = DATASIZE;
-   dims[1] = DATASIZE;
-   dataspace_id = H5Screate_simple(2, dims, NULL);
-
    for(int i=0; i < 10; i++){
+      /* Create the data space for the first dataset. */
+      dims[0] = DATASIZE;
+      dims[1] = DATASIZE;
+      dataspace_id = H5Screate_simple(2, dims, NULL);
       char buf[7];
       sprintf(buf, "/dst%d", i);
 
@@ -39,21 +38,20 @@ int main() {
 
       /* Close the first dataset. */
       status = H5Dclose(dataset_id);
-
    }
-   /* Create a dataset in group "MyGroup". */
-   dataset_id = H5Dcreate2(file_id, "/dset1", H5T_STD_I32BE, dataspace_id,
-                          H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+   // /* Create a dataset in group "MyGroup". */
+   // dataset_id = H5Dcreate2(file_id, "/dset1", H5T_STD_I32BE, dataspace_id,
+   //                        H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 
-   /* Write the first dataset. */
-   status = H5Dwrite(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT,
-                     dset1_data);
+   // /* Write the first dataset. */
+   // status = H5Dwrite(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT,
+   //                   dset1_data);
 
-   /* Close the data space for the first dataset. */
-   status = H5Sclose(dataspace_id);
+   // /* Close the data space for the first dataset. */
+   // status = H5Sclose(dataspace_id);
 
-   /* Close the first dataset. */
-   status = H5Dclose(dataset_id);
+   // /* Close the first dataset. */
+   // status = H5Dclose(dataset_id);
 
    /* Close the file. */
    status = H5Fclose(file_id);
