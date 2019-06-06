5bffd6138bd74df1df2cc3f43b6eb0b2c7f472f5
Aaron Chu <xweichu@hotmail.com>
2019-06-05 18:54:29 -0700

    update

--- plugin/provenance/hdf5creat_svc_proc.c
+++ plugin/provenance/hdf5creat_svc_proc.c
@@ -1,5 +1,6 @@
 #include "/usr/local/hdf5/include/hdf5.h"
 #include "hdf5creat.h"
+#include "pthread"
 
 int * creat_file_1_svc(char ** name, struct svc_req * req){
 	hid_t file_id = H5Fcreate(*name, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
