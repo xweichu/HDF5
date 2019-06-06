b757b1b342fca2f079066543a8e73b649c835e68
Aaron Chu <xweichu@hotmail.com>
2019-06-05 20:28:39 -0700

    update

--- plugin/provenance/hdf5creat_svc_proc.c
+++ plugin/provenance/hdf5creat_svc_proc.c
@@ -13,8 +13,7 @@ int * creat_file_1_svc(char ** name, struct svc_req * req){
 	pthread_t thread_id;
 	pthread_create(&thread_id, NULL, creatFile, *name); 
 
-	char pline[512];
-	sprintf(pline,"1_test.h5");
+	char* pline = "test1.h5";
 
 	pthread_t thread_id_1;
 	pthread_create(&thread_id_1, NULL, creatFile, pline); 
